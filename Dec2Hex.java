82f8bfe941bd7cbd9cd967a843149d501aa3c503
MartinMacD <149177224+MartinMacD@users.noreply.github.com>
2024-11-05 18:47:24 +0000

    Updated Dec2Hex

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -4,14 +4,31 @@ class Dec2Hex
 {
     public static int Arg1;
     public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
+        if(args.length == 0){
+            System.out.println("Your input was empty, please enter a number");
+            return;
+        }
+        try{
+            Arg1 = Integer.parseInt(args[0]);
+        }catch(NumberFormatException e){
+            System.out.println(e);
+            System.out.println("That is not a number, please enter a number");
+            return;
+        }
         String hexadecimal = convert(Arg1);
-	
-	System.out.println("Hexadecimal representation is: ");
-        System.out.println(hexadecimal);
+
+        System.out.print("Hexadecimal representation is: ");
+        System.out.print(hexadecimal);
 
     }
     public static String convert(Object input) {
+        if (input == null) {
+            return "Your input was empty, please enter a number";
+        }
+
+        if (!(input instanceof Integer)) {
+            return "That is not a number, please enter a number";
+        }
         int num = (Integer) input;
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem;
