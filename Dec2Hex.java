3124d5d0b6739fb2248d4a11e8276f6f370f727c
MartinMacD <149177224+MartinMacD@users.noreply.github.com>
2024-11-05 20:39:17 +0000

    Added comments

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -4,10 +4,13 @@ class Dec2Hex
 {
     public static int Arg1;
     public static void main(String args[])    {
+	//Used to check if the input is null
         if(args.length == 0){
             System.out.println("Your input was empty, please enter a number");
             return;
         }
+	//Checks if the user input is a String, returns an error message if it is a String.
+	//Otherwise converts args to an int.
         try{
             Arg1 = Integer.parseInt(args[0]);
         }catch(NumberFormatException e){
@@ -15,20 +18,25 @@ class Dec2Hex
             System.out.println("That is not a number, please enter a number");
             return;
         }
+	//Set hexadecimal to the converted number from Arg1
         String hexadecimal = convert(Arg1);
-
+	
+	//Print What the Hexadecimal representation is to the user.
         System.out.print("Hexadecimal representation is: ");
         System.out.print(hexadecimal);
 
     }
     public static String convert(Object input) {
+	//Check if the input to convert is null or empty and retun an error message if it is.
         if (input == null) {
             return "Your input was empty, please enter a number";
         }
 
+	//Check if the input isn't an integer, if it isn't then return an error message.
         if (!(input instanceof Integer)) {
             return "That is not a number, please enter a number";
         }
+	//Perform Decimal to hexadecimal calculation
         int num = (Integer) input;
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem;
@@ -41,7 +49,7 @@ class Dec2Hex
             hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
-
+	//Return that hexadecimal number
         return hexadecimal;
     }
 }
--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -7,22 +7,28 @@ public class Dec2HexTest {
     public Dec2HexTest()
    {}
     Dec2Hex dec2Hex;
-
+    //Before the testing begins, instantiate the dec2Hex object as a new Dec2Hex.
     @Before
     public void setUp() {
         dec2Hex = new Dec2Hex();
     }
 
+    //First test is to check dec2Hex.convert against a standard number,
+    //This test should result in the letter F being output and the test should be a success.
     @Test
     public void testStandardNum() {
         assertEquals("Regular number conversion should work", "F", dec2Hex.convert(15));
     }
 
+    //Second test to check dec2Hex.convert against a null/empty value.
+    //This test should result in an error message being output and the test should be a success.
     @Test
     public void testNullInput() {
     assertEquals("Your input was empty, please enter a number", dec2Hex.convert(null));
 }
 
+    //Third test is to check dec2Hex.convert against a String value.
+    //This test should result in an error message being output and the test should be a success.
     @Test
     public void testStringInput() {
     assertEquals("That is not a number, please enter a number", dec2Hex.convert("abc"));
